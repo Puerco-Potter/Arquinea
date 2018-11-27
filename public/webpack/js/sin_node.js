bd950ea82a1a42e51a6dde4fd6f56b0417821124
Puerco-Potter <Albiach92@gmail.com>
2018-11-27 10:43:20 -0300

    sin node 2

---
+++ public/webpack/js/sin_node.js
@@ -0,0 +1,6 @@
+$('.footable').footable();
+
+$('a.gallery').featherlightGallery({
+    galleryFadeIn: 100,          /* fadeIn speed when slide is loaded */
+    galleryFadeOut: 300          /* fadeOut speed before slide is loaded */
+});
